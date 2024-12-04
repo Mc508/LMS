98b189806ac798b0e6a0e9a4fdfcfa28bc2b83c3
Mc508 <mihirc508@gmail.com>
2024-12-04 07:46:51 +0530

    hero section added

---
+++ client/src/pages/student/HeroSection.jsx
@@ -0,0 +1,34 @@
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+
+const HeroSection = () => {
+  return (
+    <div className="relative bg-gradient-to-r from-blue-700 to-indigo-800 dark:from-gray-700 dark:to-gray-900 py-24 px-4 text-center ">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-white text-4xl font-bold mb-4">
+          Find the Best Coursess For You
+        </h1>
+        <p className="text-gray-200 dark:text-gray-400 mb-8">
+          Discover, learn and upskill your self with our wide range of courses
+        </p>
+        <form
+          action=""
+          className="flex items-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mb-6"
+        >
+          <Input
+            type="text"
+            placeholder="Search"
+            className=" flex-grow border-none focus-visible:ring-0 px-6 py-3 text-black dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500"
+          />
+          <Button className="bg-blue-600 dark:bg-gray-700 text-white px-6 py-3 rounded-r-full hover:bg-blue-700 ">
+            Search
+          </Button>
+        </form>
+        <Button className=" dark:bg-gray-700 bg-white text-black rounded-full hover:bg-blue-700 ">
+          Explore Courses
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default HeroSection;
