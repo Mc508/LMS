9f42edc8198255b68bb1d1bb05d82bc00acea1b2
Mc508 <mihirc508@gmail.com>
2024-12-12 23:50:41 +0530

    Edit lecture done

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -172,13 +172,88 @@ export const getCourseLecture = async (req, res) => {
   }
 };
 
-export const editLectue = async (req, res) => {
+export const editLecture = async (req, res) => {
   try {
-    const { lectureTitle, isPreviewFree } = req.body;
+    const { lectureTitle, videoInfo, isPreviewFree } = req.body;
+    const { courseId, lectureId } = req.params;
+    const lecture = await Lecture.findById(lectureId);
+    if (!lecture) {
+      return res.status(404).json({
+        message: "Lecture mot found",
+      });
+    }
+    if (lectureTitle) lecture.lectureTitle = lectureTitle;
+    if (videoInfo.videoUrl) {
+      lecture.videoUrl = videoInfo.videoUrl;
+    }
+    if (videoInfo.publicId) lecture.publicId = videoInfo.publicId;
+    if (isPreviewFree) lecture.isPreviewFree = isPreviewFree;
+
+    await lecture.save();
+    const course = await Course.findById(courseId);
+    if (course && !course.lectures.includes(lecture._id)) {
+      course.lectures.push(lecture._id);
+      await course.save();
+    }
+    return res.status(200).json({
+      lecture,
+      message: "Lecture updated",
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       message: "failed to edit lecture",
+    });
+  }
+};
+
+export const removeLecture = async (re, res) => {
+  try {
+    const { lectureId } = req.params;
+    const lecture = await Lecture.findByIdAndDelete(lectureId);
+    if (!lecture) {
+      return res.status(404).json({
+        message: "Lecture not found",
+      });
+    }
+
+    // delete lectures from cloudinarty
+
+    if (lecture.publicId) {
+      await deleteVideoFromCloudinary(lectureId);
+    }
+
+    // remove the lecture reference from the associeted course;
+
+    await Course.updateOne(
+      { lectures: lectureId },
+      { $pull: { lectures: lecture } }
+    );
+    return res.status(200).json({
+      message: "Lecture removed",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "failed to remove lecture",
+    });
+  }
+};
+
+export const getLectureById = async (req, res) => {
+  try {
+    const { lectureId } = req.params;
+    const lecture = await Lecture.findById(lectureId);
+    if (!lecture) {
+      return res.status(404).json({
+        message: "Lecture not found",
+      });
+    }
+    return res.status(200).json({
+      lecture,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "failed to find lecture by id",
     });
   }
 };
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -6,9 +6,12 @@ import {
   createCourse,
   createLecture,
   editCourse,
+  editLecture,
   getCourseById,
   getCourseLecture,
   getCreatorCourses,
+  getLectureById,
+  removeLecture,
 } from "../controllers/course.controller.js";
 
 const router = express.Router();
@@ -21,5 +24,13 @@ router
   .get(isAuthenticated, getCourseById);
 router.route("/:courseId/lecture").post(isAuthenticated, createLecture);
 router.route("/:courseId/lecture").get(isAuthenticated, getCourseLecture);
+router
+  .route("/:courseId/lecture/:lectureId")
+  .post(isAuthenticated, editLecture);
+router
+  .route("/:courseId/lecture/:lectureId")
+  .post(isAuthenticated, editLecture);
+router.route("/lecture/:lectureId").delete(isAuthenticated, removeLecture);
+router.route("/lecture/:lectureId").get(isAuthenticated, getLectureById);
 
 export default router;
